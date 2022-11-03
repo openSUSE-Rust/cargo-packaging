9701922ebd9a35ce598d7da5408e7fe67b30c62e
William Brown <wbrown@suse.de>
2022-11-04 08:45:18 +1000

    Improve support for bundle metadata

--- src/rpmprovides.rs
+++ src/rpmprovides.rs
@@ -1,25 +1,38 @@
 
-use cargo_lock::Lockfile;
+use std::path::Path;
+use tracing::debug;
+use auditable_info::{
+    audit_info_from_file,
+    Error
+};
 
-/// Run the rpm provides generator. This will take a Cargo.lock
-/// (generally put in place by the macros.cargo file)
+/// Run the rpm provides generator. This takes an elf and parses it for the
+/// appropriate rust audit header, if any.
 
 pub fn exec() {
     for line in std::io::stdin().lines() {
-        let uline = line.unwrap();
-        let lockfile = Lockfile::load(uline)
-            .expect("Unable to process lockfile");
-
-        for pkg in lockfile.packages.iter() {
-            // Rpm doesn't like - in version.
-            let version = pkg.version.to_string().replace("-", "_");
-            println!("rust-crate({}) = {}", pkg.name, version);
+        match line
+            .map_err(Error::Io)
+            .and_then(|uline| {
+                let p = Path::new(&uline);
+                audit_info_from_file(&p, Default::default()) 
+            })
+            {
+            Ok(info) => {
+                for pkg in info.packages.iter() {
+                    // Rpm doesn't like - in version.
+                    let version = pkg.version.to_string().replace("-", "_");
+                    println!("bundled(rust-crate:{}) = {}", pkg.name, version);
+                }
+            }
+            Err(Error::NoAuditData) => {
+                // May not be a rust binary, skip.
+            }
+            Err(e) => {
+                debug!(?e);
+                println!("bundled(rust-crate:BROKEN) = 9999.0.0")
+            }
         }
     }
 }
 
-
-// [  349s] warning: Invalid version (double separator '-'): 0.9.0+wasi-snapshot-preview1
-// [  349s] warning: Invalid version (double separator '-'): 0.10.0+wasi-snapshot-preview1
-// [  349s] warning: Invalid version (double separator '-'): 0.11.0+wasi-snapshot-preview1
-// [  349s] Provides: cargo-packaging-provides = 99999
